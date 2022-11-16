19480d935074de98e9e5aea4beb4466217390d35
maruiasas <118332166+maruiasas@users.noreply.github.com>
2022-11-16 16:57:37 +0900

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:dbname=practice;host=;charset=utf8', 'root', 'selVa_kiasas11');
-}   catch (PDOException $e) {
-    echo "データベース接続エラー：".$e->getMessage();
-}
-?>

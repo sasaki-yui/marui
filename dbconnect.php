0802e6f0008bbd4fca4516b875fb917fc1189049
root <root@ik1-342-31268.vs.sakura.ne.jp>
2022-11-18 17:29:43 +0900

    ファイルを削除

--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:dbname=practice;host=localhost;charset=utf8mb4', 'root', '');
-   } catch (PDOException $e) {
-    echo "データベース接続エラー：".$e->getMessage();
-
-}
-?>

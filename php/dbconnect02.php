ccf3fc9f6de01363fa5caf70071995d446ce9423
root <root@ik1-342-31268.vs.sakura.ne.jp>
2023-02-23 02:41:06 +0900

    fix02

--- php/dbconnect02.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:host=localhost;dbname=thread;charset=utf8mb4','root','selVa_kiasas11');
-   } catch (PDOException $e) {
-    echo "データベース接続エラー：".$e->getMessage();
-
-}
-?>
